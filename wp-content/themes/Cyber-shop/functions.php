3ef83bf8613a99cce7be051d8e0292108224b88f
TimmiKau <125255142+TimmiKau@users.noreply.github.com>
2024-02-28 09:19:12 +0100

    Fix Redirect Problem

--- wp-content/themes/Cyber-shop/functions.php
+++ wp-content/themes/Cyber-shop/functions.php
@@ -8,5 +8,3 @@ function mytheme_add_woocommerce_support()
     add_theme_support('woocommerce');
 }
 add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
-
-
--- wp-content/themes/Cyber-shop/page.php
+++ wp-content/themes/Cyber-shop/page.php
@@ -1,10 +1,6 @@
 <?php get_header(); ?>
 
-<div id="content">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-    <?php endwhile;
-    endif; ?>
-</div>
+<?php the_title(); ?>
+<?php the_content(); ?>
 
 <?php get_footer(); ?>
